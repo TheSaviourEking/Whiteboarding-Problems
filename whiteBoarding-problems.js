f52615c8ee244641ffbd2757ec9d2f5767305c5c
TheSaviourEking <saviour.eking@outlook.com>
2023-10-19 01:36:05 +0100

    fix logBetween()

---
+++ whiteBoarding-problems.js
@@ -0,0 +1,71 @@
+const logBetween = (lowNum, highNum) => {
+    let result = [];
+    if (lowNum <= highNum) {
+        for (let i = lowNum; i <= highNum; i++) {
+            result.push(i);
+        }
+    }
+    return result;
+}
+
+module.exports = {
+    logBetween,
+    // logBetweenStepper,
+    // printReverse,
+    // fizzBuzz,
+    // isPrime,
+    // maxValue,
+    // myIndexOf,
+    // factorArray,
+    // oddRange,
+    // reverseHyphenString,
+    // intersect,
+    // mirrorArray,
+    // abbreviate,
+    // adults,
+    // countScores,
+    // firstNPrimes,
+    // peakFinder,
+    // divisibleByThreePairSum,
+    // zipArray,
+    // twoDimensionalTotal,
+    // countInnerElement,
+    // twoDiff,
+    // arrayDiff,
+    // valueCounter,
+    // elementCount,
+    // nextTwoPrimes,
+    // pairProduct,
+    // twoDimensionalSize,
+    // longWordCount,
+    // factorial,
+    // lcm,
+    // hipsterfyWord,
+    // hipsterfy,
+    // objectToString,
+    // shortestWord,
+    // greatestCommonFactor,
+    // isPassing,
+    // valueConcat,
+    // threeInArrow,
+    // variableNameify,
+    // threeIncreasing,
+    // reverse2D,
+    // reverb,
+    // countRepeats,
+    // pairsToString,
+    // countAdjacentSums,
+    // signFlipCount,
+    // powerSequence,
+    // collapseString,
+    // oddWordsOut,
+    // mindPsAndQs,
+    // commonFactors,
+    // commonPrimeFactors,
+    // splitHalfWay,
+    // threeUniqueVowels,
+    // vowelShift,
+    // hasSymmetry,
+    // numsToWords,
+    // moreDotLessDash
+}
